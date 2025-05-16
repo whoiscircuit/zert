1730c9c7d0f6d5e759ad9a1eff52f1af7ef76103
oxcl <oxcl@proton.me>
2025-05-16 16:25:04 +0330

    revert changes in zert.plugin.zsh

--- zert.plugin.zsh
+++ zert.plugin.zsh
@@ -1,7 +1,3 @@
 #!/usr/bin/env zsh
-[ -z "$ZERT_DIR" ] && export ZERT_PULGINS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zert"
-
-export ZERT_PLUGINS_DIR="${ZERT_DIR}/plugins"
-export ZERT_FILE="${ZERT_DIR}/zert.ini"
-export ZERT_LOCKFILE="${ZERT_DIR}/zert.lock"
-
+fpath=("$ZERT_PLUGINS_DIR/zert/functions" $fpath)
+autoload -Uz zert-load zert-update zert-clean zert-use zert-fetch zert-log
