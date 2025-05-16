6a58b16b79b2a5700315cdb13ae7cb988977c710
oxcl <oxcl@proton.me>
2025-05-16 18:06:18 +0330

    add ZERT_PLUGINS_LOADED array

--- zert-bootstrap.zsh
+++ zert-bootstrap.zsh
@@ -5,10 +5,11 @@
 #   [ -f "${ZERT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/zert}/zert.pulgin.zsh" ] && source "${ZERT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/zert}/zert-bootstrap.zsh" || source <(curl -fsSL https://raw.githubusercontent.com/whoiscircuit/zert/main/zert-bootstrap.zsh)
 
 # Zert configuration variables
-[ -z "$ZERT_REPO"        ] && export ZERT_REPO="https://github.com/whoiscircuit/zert"
-[ -z "$ZERT_DIR"         ] && export ZERT_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zert"
-[ -z "$ZERT_PLUGINS_DIR" ] && export ZERT_PLUGINS_DIR="${ZERT_DIR}/plugins"
-[ -z "$ZERT_LOCKFILE"    ] && export ZERT_LOCKFILE="${ZERT_DIR}/zert.lock"
+[ -z "$ZERT_REPO"           ] && export ZERT_REPO="https://github.com/whoiscircuit/zert"
+[ -z "$ZERT_DIR"            ] && export ZERT_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zert"
+[ -z "$ZERT_PLUGINS_DIR"    ] && export ZERT_PLUGINS_DIR="${ZERT_DIR}/plugins"
+[ -z "$ZERT_LOCKFILE"       ] && export ZERT_LOCKFILE="${ZERT_DIR}/zert.lock"
+[ -z "$ZERT_NOTIFY_UPDATES" ] && export ZERT_NOTIFY_UPDATES=1
 
 # Colors for output
 ERROR='\033[0;31m[ZERT]: '
