e08b581f6e5f324f236aa84cbeebc6aeac9304eb
oxcl <oxcl@proton.me>
2025-05-16 17:43:22 +0330

    implement a basic zert-load function

--- zert.plugin.zsh
+++ zert.plugin.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 fpath=("$ZERT_PLUGINS_DIR/zert/functions" $fpath)
-autoload -Uz zert-load zert-update zert-clean zert-use zert-fetch zert-log
+autoload -Uz zert-load zert-update zert-clean zert-use zert-fetch zert-log zert-id
